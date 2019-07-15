ae15bed2626eb752fcabfa756ae1946850b8d101
Zurglots <bfeole@gmail.com>
2019-07-15 17:11:36 -0400

    All endpoint printing data

---
+++ src/main/java/com/countries/javacountries/CountryController.java
@@ -0,0 +1,31 @@
+package com.countries.javacountries;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/names")
+//@RequestMapping("/population")
+public class CountryController // defines which endpoints handle specific actions
+{
+    @GetMapping(value= "/all",
+                produces = {"application/json"})
+    public ResponseEntity<?> getAllCountries()
+    {
+        return new ResponseEntity<>(JavacountriesApplication.ourCountryList.countryList, HttpStatus.OK);
+    }
+
+//    @GetMapping(value ="/start/{letter}",
+//                produces = {"application/json"})
+//
+//    public ResponseEntity<?> getCountryByLetter(@PathVariable String )
+//    {
+//
+//    }
+}
+
+
